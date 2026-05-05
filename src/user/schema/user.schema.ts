922836f7336d055fbc5bfd5a23b96e0fd03e8ea9
lilstex <lilstex4good@gmail.com>
2026-05-05 13:12:45 +0100

    fix: clarify awards description as optional in User schema

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -42,7 +42,7 @@ export class User extends Document {
   @Prop({ type: [Project] }) projects: Project[];
   @Prop([{ language: String, proficiency: String }])
   languages: { language: string; proficiency: string }[];
-  @Prop([{ title: String, issuer: String, date: String, description: String }])
+  @Prop([{ title: String, issuer: String, date: String, description: { type: String } }])
   awards: { title: string; issuer: string; date: string; description?: string }[];
   @Prop([{ organization: String, role: String, startDate: String, endDate: String, description: String }])
   volunteerWork: { organization: string; role: string; startDate: string; endDate: string; description: string }[];
